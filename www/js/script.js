37c63ef2df2c31b72b6158da653f3410aad85085
MyApp <myapp@gmail.com>
2026-06-04 10:28:37 +0330

    fix calculator buttons

--- www/js/script.js
+++ www/js/script.js
@@ -1,11 +1,15 @@
 const display = document.getElementById("display");
 
-function append(value){
+function appendValue(value){
     display.value += value;
 }
 
 function clearDisplay(){
     display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
 }
 
 function calculate(){
